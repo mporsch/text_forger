e4c4cbe6606f14082a691f11248a7b84d52c364a
Marco Porsch <marco.porsch@posteo.de>
2017-07-23 17:21:08 +0200

    basic training

--- text_forger.cpp
+++ text_forger.cpp
@@ -1,30 +1,116 @@
 #include <csignal>
 #include <iostream>
-#include <vector>
 #include <map>
+#include <fstream>
+#include <stdexcept>
+#include <cctype>
+#include <numeric>
+
+#define DEBUG_TRAINING
 
 struct WordReference {
-  std::string word;
   unsigned count;
 };
-using WordReferenceList = std::vector<WordReference>;
+using WordReferenceMap = std::map<std::string, WordReference>;
 
 struct WordMapEntry {
-  WordReferenceList refs;
+  WordReferenceMap refs;
   unsigned refSum;
 };
 using WordMap = std::map<std::string, WordMapEntry>;
 
-void TrainFrom(WordMap &wordMap, char const *arg) {
+enum CharType {
+  Space
+, Letter
+, Number
+, Special
+};
 
+CharType GetCharType(char c) {
+  if(std::isspace(c)) {
+    return Space;
+  } else if(std::isalpha(c)) {
+    return Letter;
+  } else if(std::isalnum(c)) {
+    return Number;
+  } else {
+    return Special;
+  }
+}
+
+void TrainFromLine(WordMap &wordMap, std::string const &line) {
+  if(line.empty()) {
+    return;
+  }
+
+  wordMap["\n"];
+  std::string lastToken = "\n";
+  size_t tokenStart = 0;
+  auto lastCharType = GetCharType(line.at(0));
+  for(size_t pos = 1; pos < line.length(); ++pos) {
+    auto const charType = GetCharType(line.at(pos));
+    if(charType != lastCharType) {
+      auto const token = line.substr(tokenStart, pos - tokenStart);
+
+      if(!token.empty()) {
+        wordMap[token];
+
+        ++wordMap.at(lastToken).refs[token].count;
+
+        lastToken = token;
+      }
+
+      tokenStart = pos + (charType == Space ? 1 : 0);
+    }
+    lastCharType = charType;
+  }
+
+  auto const token = line.substr(tokenStart);
+  if(!token.empty()) {
+    wordMap[token];
+    ++wordMap.at(lastToken).refs[token].count;
+  }
+}
+
+void TrainFrom(WordMap &wordMap, char const *arg) {
+  std::ifstream ifs(arg);
+  if(!ifs.is_open()) {
+    throw std::runtime_error("Failed to open " + std::string(arg));
+  }
+
+  std::string line;
+  while(std::getline(ifs, line)) {
+    TrainFromLine(wordMap, line);
+  }
 }
 
 unsigned TrainFinalize(WordMap &wordMap) {
-
+  unsigned sumsum = 0;
+  for(auto &&p : wordMap) {
+    auto const sum = std::accumulate(
+      std::begin(p.second.refs), std::end(p.second.refs), 0,
+      [](unsigned sum, WordReferenceMap::value_type const &p) -> unsigned {
+        return sum + p.second.count;
+      });
+      p.second.refSum = sum;
+      sumsum += sum;
+  }
+  return sumsum;
 }
 
-std::string Forge(unsigned wordCount) {
+void DumpWordMap(WordMap const &wordMap) {
+  for(auto &&wordMapPair : wordMap) {
+    std::cout << wordMapPair.first << ": ";
+    for(auto &&wordReferenceMapPair : wordMapPair.second.refs) {
+      std::cout << wordReferenceMapPair.first
+        << " (" << wordReferenceMapPair.second.count << ") ";
+    }
+    std::cout << '\n';
+  }
+}
 
+std::string Forge(unsigned wordCount, WordMap const &wordMap) {
+  return "";
 }
 
 void Exit(int) {
@@ -43,10 +129,13 @@ int main(int argc, char **argv) {
   } else {
     // train word map from user input
     WordMap wordMap;
-    for(auto arg = argv[1]; arg < argv[argc]; ++arg) {
-      TrainFrom(wordMap, arg);
+    for(auto arg = argv + 1; arg < argv + argc; ++arg) {
+      TrainFrom(wordMap, *arg);
     }
     auto const refCountSum = TrainFinalize(wordMap);
+#ifdef DEBUG_TRAINING
+    DumpWordMap(wordMap);
+#endif
     std::cout << "Trained word map with "
       << wordMap.size() << " entries with "
       << refCountSum << " references\n\n";
@@ -61,7 +150,7 @@ int main(int argc, char **argv) {
       } else if(wordCount == 0) {
         break;
       } else {
-        std::cout << '\n' << Forge(static_cast<unsigned>(wordCount)) << "\n\n";
+        std::cout << '\n' << Forge(static_cast<unsigned>(wordCount), wordMap) << "\n\n";
       }
     }
 
